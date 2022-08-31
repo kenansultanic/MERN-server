5d715607e0c921c7d19af4e0944fb81a2b527e88
unknown <ganicch>
2022-08-31 19:33:02 +0200

    The API was added and the password was hashed

--- api/users-api.js
+++ api/users-api.js
@@ -1,25 +1,29 @@
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 const users = {
     addNewUser: async (req, res, next) => {
-        const user = new User({
-            email: req.body.email,
-            username: req.body.username,
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            // TODO(Hash password)
-            password: req.body.password,
-            // TODO(ADD DEFAULT PFP URL)
-            pfp_url: ''
-        })
-        try {
-            const dataToSave = await data.save();
-        }
-        catch (err) {
-            console.log(err)
-        }
-        next()
-    },
+        const pass = req.body.password
+        bcrypt.hash(pass,saltRounds, async (err,hash) => {
+            const user = new User({
+                email: req.body.email,
+                username: req.body.username,
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                password: hash,
+                pfp_url: `https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80`
+            })
+            try {
+                const dataToSave = await user.save();
+            }
+            catch (err) {
+                console.log(err)
+            }
+            next()
+        
+        })},
+
     getAllUsers: async (req, res, next) => {
         try {
             req.users  = await User.find()
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const index = require('./routes/index')
+const users = require('./routes/users')
 const app = express();
 
 require('dotenv').config()
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use('/', index)
+app.use('/user', users)
 
 const PORT = process.env.PORT
 const server = app.listen(PORT, () => {
